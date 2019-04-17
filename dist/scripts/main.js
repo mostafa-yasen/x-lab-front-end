19a443ac0bf638c296073ac8705cb2a7e511423c
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-04-17 03:38:10 +0200

    Added some blugins (typed.js, jquery, bootstrap-grid, magnific-popup) and included the font from google 'Fredoka One'.

---
+++ dist/scripts/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.test', {
+    strings: ["", "First sentence.", "Second sentence.","Hello sentence.", "This is sentence."],
+    typeSpeed: 30,
+    loop: true,
+    backspeed: 70
+  });
